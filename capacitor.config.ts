22bc1836f70ca049db0d89b69bbe030427695297
Carlos Sousa <arlo@gmail252.com>
2023-04-14 17:09:44 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movieApp-02',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
